2cc72b7c122ac1f78ef7ceec1d34d352eeff9fc0
kenzie <webstersevenlanguages@gmail.com>
2023-05-12 17:13:06 -0300

    first commit

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,75 @@
+import './App.css';
+import Cards from './componentes/Cards/Cards';
+import style from './App.module.css';
+import NavBar from './componentes/NavBar/NavBar';
+import {useState , useEffect} from 'react';
+import axios from 'axios'
+import { Routes, Route, useLocation, useNavigate } from "react-router-dom";
+import About from "./componentes/About";
+import Detail from "./componentes/Detail";
+import Forms from "./componentes/Form";
+import Favorites from "./componentes/Favorites";
+const EMAIL = 'webstersevenlanguages@gmail.com';
+const PASSWORD = 'asd123';
+
+
+function App() {
+   const [characters, setCharacters] = useState([]);
+   const {pathname} = useLocation();
+ 
+ 
+   const navigate = useNavigate();
+   const [access, setAccess] = useState(false);
+ 
+ 
+ function login(userData) {
+    if (userData.password === PASSWORD && userData.email === EMAIL) {
+       setAccess(true);
+       navigate('/home');
+    }
+ };
+ 
+ useEffect(() => {
+   !access && navigate('/');
+ }, [access]);
+   
+ 
+ 
+   function onSearch(id) {
+     axios(`https://rickandmortyapi.com/api/character/${id}`).then(
+       ({ data }) => {
+         if (data.name) {
+           setCharacters((oldChars) => [...oldChars, data]);
+         } else {
+           window.alert("¡No hay personajes con este ID!");
+         }
+       }
+     );
+   }
+ 
+   const onClose = (id) => {
+     setCharacters(
+       characters.filter((char) => {
+         return char.id !== Number(id);
+       })
+     );
+   };
+ 
+   return (
+ 
+     <div className={style.App}>
+      {pathname !== '/' && <NavBar onSearch={onSearch} />}
+ 
+          <Routes>
+             <Route path="/" element={<Forms login={login}/>}/>
+             <Route path="/home" element={<Cards characters={characters} onClose={onClose} />}/>
+             <Route path="/about" element={<About/>}/>
+             <Route path="/detail/:id" element={<Detail/>}/>
+             <Route path="/favorites" element={<Favorites/>}/>
+          </Routes>
+ 
+     </div>
+   );
+ }
+ 
+export default App;
